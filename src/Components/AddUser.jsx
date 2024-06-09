e8baf929c90d0c74ba1028d35131668a449abeb8
Manas Bhola <manasbhola@gmail.com>
2024-06-09 15:07:54 +0530

    Revision Project - Error Modal

--- src/Components/AddUser.jsx
+++ src/Components/AddUser.jsx
@@ -1,13 +1,62 @@
-import React from "react";
-import Form from "./Form";
+import React, { useRef, useState } from "react";
 import Card from "./Card";
-import UserList from "./UserList";
+import Button from "./Button";
+import "./AddUser.css";
+import ErrorModal from "./ErrorModal";
 
 const AddUser = ({ onAddUser }) => {
+  const userName = useRef("");
+  const age = useRef("");
+  const [error, setError] = useState();
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    const userNameVal = userName.current.value;
+    const ageVal = age.current.value;
+    if (
+      userNameVal.trim().length == 0 ||
+      ageVal.trim().length == 0 ||
+      +ageVal < 1
+    ) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid input",
+      });
+      return;
+    } else {
+      onAddUser(userNameVal, ageVal);
+    }
+
+    userName.current.value = "";
+    age.current.value = "";
+  };
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
-    <Card>
-      <Form onAddFormUser={onAddUser} />
-    </Card>
+    <div>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onError={errorHandler}
+        />
+      )}
+      <Card className="input">
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">UserName</label>
+          <input type="text" id="username" ref={userName} />
+
+          <label htmlFor="age">Age</label>
+          <input type="number" id="age" ref={age} />
+
+          <Button type="submit">Add User</Button>
+          {/* <Button onClick={clickHandler}>Click Here</Button> */}
+        </form>
+      </Card>
+    </div>
   );
 };
 
--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-
+import "./Button.css";
 const Button = (props) => {
   return (
     <button
-      className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+      className="button"
       type={props.type || "button"}
+      onClick={props.onClick}
     >
       {props.children}
     </button>
